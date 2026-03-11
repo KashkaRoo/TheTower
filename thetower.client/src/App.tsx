88dd959d723d951f33d86257a406c36171b1d8dc
aastapaasta <vampirepasta@gmail.com>
2026-03-11 15:42:39 -0700

    textual edits

--- thetower.client/src/App.tsx
+++ thetower.client/src/App.tsx
@@ -109,7 +109,7 @@ function App() {
                         <i>
                             The night grows old, and you are weary. Your waking hours have been spent in service to the Tower, aiding your coven. Bleary-eyed, you climb the Tower stairs, feet dragging, thinking only of sleep.
                             <p></p>
-                            At last, you reach your chambers. You kick off your shoes and collapse into bed. But just as you begin to drift, the Tower beckons—it is not finished with you yet.
+                            At last, you reach your chambers. You kick off your shoes and collapse into bed. But just as you begin to drift off to sleep, the Tower beckons—it is not finished with you yet.
                             <p></p>
                             You find yourself in a dream where things are not as they seem in the waking world, and you have been given a task. You steady yourself, get your bearings, and in your mind, the Tower whispers your mission.
                             <p></p>
